0fc0ad47bb10458b7d1dc5cfde6606c727f8a299
Siddharth Anand <siddharth21494@iiitd.ac.in>
2024-09-05 23:26:05 +0530

    new3

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ connectDB();
 
 // Api endpoints
 // app.use("/api/food", foodRouter)
-// app.use("/images", express.static('uploads'))
+app.use("/images", express.static('uploads'))
 app.use("/api/user",userRouter)
 app.use("/api/cart",cartRouter)
 app.use("/api/order", orderRouter)
