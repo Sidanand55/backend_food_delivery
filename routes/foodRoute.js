a09527d734b6c3d75a1391335b601e38bccdd90f
Siddharth Anand <siddharth21494@iiitd.ac.in>
2024-09-05 23:25:02 +0530

    new2

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,26 +1,26 @@
-import express from "express";
-import { addFood, listFood, removeFood} from "../controllers/foodController.js";
-import multer from "multer"
+// import express from "express";
+// import { addFood, listFood, removeFood} from "../controllers/foodController.js";
+// import multer from "multer"
 
 
-const foodRouter= express.Router();
+// const foodRouter= express.Router();
 
-// Image storage engine
+// // Image storage engine
 
-const storage= multer.diskStorage({
-    destination:"uploads",
-    filename:(req,file,cb)=>{
-        return cb(null,`${Date.now()}${file.originalname}`)     //file name will become unique
-    }
-})
+// const storage= multer.diskStorage({
+//     destination:"uploads",
+//     filename:(req,file,cb)=>{
+//         return cb(null,`${Date.now()}${file.originalname}`)     //file name will become unique
+//     }
+// })
 
-const upload= multer({storage: storage});
+// const upload= multer({storage: storage});
 
-foodRouter.post("/add",upload.single("image"),addFood)
+// foodRouter.post("/add",upload.single("image"),addFood)
 
-foodRouter.get("/list", listFood)
+// foodRouter.get("/list", listFood)
 
-foodRouter.post("/remove", removeFood)
+// foodRouter.post("/remove", removeFood)
 
 
 
